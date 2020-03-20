b4ccb587c8351450a84b7e46388323a5f79ec251
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-20 01:00:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,6 +34,14 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  ret_hash = []
+  col = 0 
+  while movies_collection[col] do 
+    ret_hash[col]= movie_with_director_name(name, movies_collection[col])  
+    col += 1
+  end
+  ret_hash
+end
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
